2340ca4aacd9700a8312322dcbb9c84b7a8ec46e
wellsilver <wellsilver2016@gmail.com>
2022-02-18 17:48:11 -0500

    Update grass.py

--- grass.py
+++ grass.py
@@ -3,6 +3,9 @@ client = discord.Client()
 token = 'OTQ0MzU0ODQyOTYzNTAxMDg3.YhAY-w.BrDq1UIRW5btYxSt7VEkkMDTIzo'
 
 @client.event
+async def on_ready():
+    print("Bot started")
+@client.event
 async def on_message(i):
     c = i.content.lower()
     if i.author.bot:
