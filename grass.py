0c97c4d0517b61edf161e9bb4e0c7fbb836d30c1
wellsilver <wellsilver2016@gmail.com>
2022-02-18 17:30:10 -0500

    Update grass.py

--- grass.py
+++ grass.py
@@ -1,6 +1,6 @@
 import discord
 client = discord.Client()
-token = 'token'
+token = 'OTQ0MzU0ODQyOTYzNTAxMDg3.YhAY-w.BrDq1UIRW5btYxSt7VEkkMDTIzo'
 
 @client.event
 async def on_message(i):
@@ -9,10 +9,10 @@ async def on_message(i):
         print()
     else:
         if 'genshin' in c:
-            await i.reply("Touch some grass")
+            await i.channel.send("Touch some grass", delete_after=5)
         if 'nft' in c:
-            await i.reply("Touch some grass")
+            await i.channel.send("Touch some grass", delete_after=5)
         if 'league of legends' in c:
-            await i.reply("Touch some grass")
+            await i.channel.send("Touch some grass", delete_after=5)
 
 client.run(token)
