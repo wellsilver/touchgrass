f8572caada1d51a676adc487c527a1d3cf900b38
wellsilver <wellsilver2016@gmail.com>
2022-02-18 20:49:03 -0500

    Update grass.py

--- grass.py
+++ grass.py
@@ -1,17 +1,25 @@
 import discord
+import json
+import time
+
 client = discord.Client()
-token = 'OTQ0MzU0ODQyOTYzNTAxMDg3.YhAY-w.BrDq1UIRW5btYxSt7VEkkMDTIzo'
-y = json.loads(open("a.json", "r"))
+token = 'NzU5ODc4NDIzMzY2OTkxODcy.X3D57Q._MVhPWH56oYrgvusXuCYuF2bKcM'
+f = open('a.json',)
+data = json.load(f)
 @client.event
 async def on_ready():
     print("Bot started")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f'Just restarted! V1.1'))
+    
 @client.event
-async def on_message(i):
-    c = i.content.lower()
-    if i.author.bot:
+async def on_message(l):
+    c = l.content.lower()
+    if l.author.bot:
         print()
     else:
-        for i in y:
+        for i in data['names']:
+            print(i)
             if i in c:
-                await i.channel.send(f"Touch some grass ", delete_after=30)
+                await l.channel.send(f"Touch some grass {l.author.mention}", delete_after=30)
+                return
 client.run(token)
