fffe0e9455498d75aff10efa09fee4cca9898bc9
wellsilver <wellsilver2016@gmail.com>
2022-02-19 10:37:55 -0500

    Update grass.py

--- grass.py
+++ grass.py
@@ -9,9 +9,9 @@ data = json.load(f)
 @client.event
 async def on_ready():
     print("Bot started")
-    await client.bot.change_presence(game=discord.Game(name="Just restarted! v1.2", type=1))
+    await client.change_presence(game=discord.Game(name="Just restarted! v1.2", type=1))
     time.sleep(90)
-    await client.bot.change_presence(game=discord.Game(name="V1.2", type=1))
+    await client.change_presence(game=discord.Game(name="V1.2", type=1))
     
 @client.event
 async def on_message(l):
